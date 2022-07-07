73dc336dcc1ee7a92c8289738c5f14985ca34ed7
glody007 <glodymbutwile@gmail.com>
2022-07-07 12:53:29 +0200

    fix: form handler

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,5 +1,8 @@
 function isUrl(formText) {
-    return formText.match(/(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})/)
+    if(formText.match(/(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})/)) {
+        return true
+    }
+    return false
 }
 
 function handleSubmit(event) {
